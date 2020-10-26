e78b40d2de7c0047aa5355a199bcd852f3429944
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-26 18:23:08 +0100

    - added UnitOfMeasure Entity - Add One To One JPA Relationships

---
+++ src/main/java/guru/morodev/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,38 @@
+package guru.morodev.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Luca Moro on 26/10/2020 18:14
+ */
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    //private String uom;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
