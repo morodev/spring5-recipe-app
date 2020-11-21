37bebccfec510402b542894aefdcb033cab32637
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-11-21 13:33:51 +0100

    adding en lenguage add 400 and 404 error page

--- src/main/java/guru/morodev/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/morodev/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.morodev.spring5recipeapp.controllers;
 
 import guru.morodev.spring5recipeapp.commands.RecipeCommand;
+import guru.morodev.spring5recipeapp.exceptions.NotFoundException;
 import guru.morodev.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created By Luca Moro on 7/11/2020  16:53
@@ -53,5 +56,21 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
     }
 }
